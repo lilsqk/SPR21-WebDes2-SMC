30671c90b700c24e7ebceed51916e34b0f1da537
lilsqk <marinvesely@gmail.com>
2021-05-09 23:16:27 -0700

    added image toggle to in class javascript example

--- Week10/in_class/script.js
+++ Week10/in_class/script.js
@@ -1,7 +1,9 @@
 let header = document.getElementById("myHeader");
 let clrBTN = document.getElementById("colorBTN");
+let cuteImg = document.getElementById("cutie");
+let tgglBTN = document.getElementById("toggleBTN")
 
-let changeTheColor = function(){
+function changeTheColor(){
     // COLOR CHANGING HERE
     let redComp = Math.random() * 255;
     let greenComp = Math.random() * 255;
@@ -13,4 +15,25 @@ let changeTheColor = function(){
     header.style.backgroundColor = "rgb("+ redComp + ", "+ greenComp + ", "+ blueComp +")";
 }
 
+let toggleTheImage = function(){
+    // IMAGE TOGGLING HERE
+
+    if (tgglBTN.innerText === "Now show the second one!") {
+        console.log("show the second image")
+        cuteImg.src = "pch.jpeg";
+        tgglBTN.innerText = "Now show the first one!";
+    }
+    else {
+        console.log("show the first image");
+        cuteImg.src = "cute.jpeg";
+        tgglBTN.innerText = "Now show the second one!";
+
+        //if it's false
+    }
+    
+}
+
+changeTheColor();
+
+tgglBTN.addEventListener("click", toggleTheImage);
 clrBTN.addEventListener("click", changeTheColor);
